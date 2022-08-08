4b407334145422523c45f0541f8a39f3956f3403
paula <106301749+paulavaschetto@users.noreply.github.com>
2022-08-08 17:26:09 +1200

    final validation form with jquery plugin

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-function validateForm() {
-    let x = document.forms["myForm"]["firstName"].value;
-    if (x == "") {
-      alert("First name can't be blank");
-      return false;
+// Wait for the DOM to be ready
+$(function() {
+  // Initialize form validation on the registration form.
+  // It has the name attribute "registration"
+  $("form[name='registration']").validate({
+    // Specify validation rules
+    rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      firstname: "required",
+      lastname: "required",
+      email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      },
+      phonenumber: {
+        required: true,
+        minlength: 10
+      }
+    },
+    // Specify validation error messages
+    messages: {
+      firstname: "Please enter your firstname",
+      lastname: "Please enter your lastname",
+      password: {
+        required: "Please provide your phone number",
+        minlength: "Please enter a valid phone number"
+      },
+      email: "Please enter a valid email address"
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
     }
-  }
+  });
+});
